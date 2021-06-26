a27f2967278cc55c95682b82c05ff7c380989c08
jordanscff <54541513+jordanscff@users.noreply.github.com>
2021-06-26 11:11:39 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 'use strict';
-fetch('https://api.kanye.rest')
+let quotesDiv = document.getElementById('quotes-div')
+fetch('https://type.fit/api/quotes')
 .then(res => res.json())
 .then(quote => {
-    quotesDiv.innerHTML =  `<p> ${quote.quote} </p>`
+    quotesDiv.innerHTML =  `<p> ${quote[0].text} </p>`
 })
 
 //Get Quote
 let quotes = document.getElementById('quotes')
 quotes.addEventListener("click", evt => {
-  let quotesDiv = document.getElementById('quotes-div')
-  fetch('https://api.kanye.rest')
+  fetch('https://type.fit/api/quotes')
   .then(res => res.json())
   .then(quote => {
-      quotesDiv.innerHTML =  `<p> ${quote.quote} </p>`
+      const listNumber = getRandomInt(quote.length-1)
+      quotesDiv.innerHTML =  `<p> ${quote[listNumber].text} </p>`
   })
 })
+
+//Random function
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
