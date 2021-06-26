731352062825d7ee6d805f4c29085a430d129882
jordanscff <54541513+jordanscff@users.noreply.github.com>
2021-06-26 16:50:06 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 'use strict';
+let quotes = document.getElementById('quotes')
+let advices = document.getElementById('advices')
 let quotesDiv = document.getElementById('quotes-div')
-fetch('https://type.fit/api/quotes')
-.then(res => res.json())
-.then(quote => {
-    quotesDiv.innerHTML =  `<p> ${quote[0].text} </p>`
-})
+quotesDiv.innerHTML =  `<p>Choose selection above</p>`
+
 
 //Get Quote
-let quotes = document.getElementById('quotes')
 quotes.addEventListener("click", evt => {
   fetch('https://type.fit/api/quotes')
   .then(res => res.json())
@@ -15,7 +13,16 @@ quotes.addEventListener("click", evt => {
       const listNumber = getRandomInt(quote.length-1)
       quotesDiv.innerHTML =  `<p> ${quote[listNumber].text} </p>`
   })
-})
+});
+
+//Get Advice
+advices.addEventListener("click", evt => {
+  fetch('https://api.adviceslip.com/advice')
+  .then(res => res.json())
+  .then(advice => {
+      quotesDiv.innerHTML =  `<p> ${advice.slip.advice} </p>`
+  })
+});
 
 //Random function
 function getRandomInt(max) {
